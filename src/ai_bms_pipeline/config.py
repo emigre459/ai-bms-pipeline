da52b65ca87daa1063d12288d9ea308130139a90
Dave Rench McCauley <11720378+emigre459@users.noreply.github.com>
2026-03-11 13:47:06 -0400

    Add basic logging

---
+++ src/ai_bms_pipeline/config.py
@@ -0,0 +1,13 @@
+LOGGER_NAME = "ai_bms_pipeline"
+LOGGER_FORMAT = "%(asctime)s: %(levelname)s (%(name)s:%(module)s.%(funcName)s:L%(lineno)d) - %(message)s"
+DATETIME_FORMAT = "%Y-%m-%d_T%H_%M_%S%Z"
+
+# Taken from https://www.color-hex.com/color-palette/1041937
+# <a target="_blank" href="https://icons8.com/icon/7880/location">Location</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+BAD_TO_GOOD_HEX_TO_RGB_COLORS = {
+    "af1c17": (175, 28, 23),  # 0-20
+    "cf7673": (207, 118, 115),  # 20-40
+    "a1a38c": (161, 163, 140),  # 40-60
+    "8bd7b3": (139, 215, 179),  # 60-80
+    "17af68": (23, 175, 104),  # 80-100
+}
